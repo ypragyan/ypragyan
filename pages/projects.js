71217b6ee9b01d5c3b00cfc981d801489c8c1afb
Pragyan Yadav <ypragyan2021@gmail.com>
2025-08-21 18:39:38 -0400

    imgs

--- pages/projects.js
+++ pages/projects.js
@@ -8,35 +8,35 @@ export default function Projects() {
       subtitle: "UConn Galaxy Formation Group",
       description:
         "We use saliency maps to analyze neural networks trained on large-scale cosmological simulations, revealing which features drive predictions and improving transparency and reliability in cosmology.",
-      image: "/XAI.png",
+      image: "./XAI.png",
     },
     {
       title: "Network Theory Analysis of the Cosmic Web",
       subtitle: "UConn Galaxy Formation Group",
       description:
         "Analyzing the structure of the cosmic web in cosmological simulations through a network theory perspective, and investigating the properties of dark matter halos within the large-scale structure of the universe.",
-      image: "/LSS.png",
+      image: "./LSS.png",
     },
     {
       title: "Exploring Solar Complexity through Image Analysis",
       subtitle: "Wolfram Student Ambassador",
       description:
         "Developed Wolfram Language code for analyzing high-resolution solar images. Analyzed fractal dimension to study the complexity of solar flares. Working on advanced image processing techniques for solar physics research.",
-      image: "/Fractal.jpg",
+      image: "./Fractal.jpg",
     },
     {
       title: "Probing Solar Flare Dynamics via Explainable AI Techniques",
       subtitle: "Independent Research",
       description:
         "Wrote code to process space weather data and solar imagery. Implemented machine learning models to predict space weather events. Discovered unique pre-flaring patterns across different solar wavelengths using explainable AI techniques.",
-      image: "/Solar.png",
+      image: "./Solar.png",
     },
     {
       title: "Analyzing Cosmic Rays Using the Pixet Particle Detector",
       subtitle: "Wolfram High School Summer Research Program",
       description:
         "Employed Wolfram Language to process data from the Pixet particle detector. Designed and conducted experiments to detect and analyze subatomic particles. Developed algorithms for particle detection and cosmic ray analysis.",
-      image: "/3D.png",
+      image: "./3D.png",
     },
   ];
 
