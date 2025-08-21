b051016dfac6b2cacbca7c572e806abc952086a6
Pragyan Yadav <ypragyan2021@gmail.com>
2025-08-21 18:17:57 -0400

    fixed images

--- next.config.js
+++ next.config.js
@@ -1,10 +1,15 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
     domains: ['img.youtube.com'],
-  }
+  },
+  basePath: isProd ? '/ypragyan' : '',
+  assetPrefix: isProd ? '/ypragyan/' : '',
+
 }
 
 module.exports = nextConfig
