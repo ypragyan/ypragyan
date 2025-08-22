eb58f097d80c4bf8762156c22d00fe07b3ad7b9b
Pragyan Yadav <ypragyan2021@gmail.com>
2025-08-21 22:08:02 -0400

    hi

--- next.config.js
+++ next.config.js
@@ -1,14 +1,10 @@
 /** @type {import('next').NextConfig} */
-
-const isProd = process.env.NODE_ENV === 'production'
-
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   output: 'export',
   images: { unoptimized: true },
-  basePath: isProd ? '/ypragyan' : '',
-  assetPrefix: isProd ? '/ypragyan/' : '',
+  assetPrefix: './', 
 }
 
 module.exports = nextConfig
